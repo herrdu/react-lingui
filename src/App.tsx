a3077a26cb01371dc4d94f94dc412e71ed5039b5
yutao.du <yutao.du@nio.com>
2023-05-30 15:17:21 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { i18n } from "@lingui/core";
+import { I18nProvider } from "@lingui/react";
+import { Trans } from "@lingui/macro";
+
+export async function dynamicActivate(locale: string) {
+  const { messages } = await import(
+    // @ts-ignore
+    `./file.js!=!@lingui/loader!./locales/en/messages.po`
+  );
+
+  console.log("messages", messages);
+
+  i18n.load(locale, messages);
+  i18n.activate(locale);
+}
 
 function App() {
+  useEffect(() => {
+    setTimeout(() => {
+      dynamicActivate("en");
+    }, 0);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <I18nProvider i18n={i18n}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        <h1>
+          <Trans>Message Inbox</Trans>
+        </h1>
+      </div>
+    </I18nProvider>
   );
 }
 
